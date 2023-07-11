0a14f94bec6992d4f20be89c72a1fa9381e32c3d
Alfonso <alfonso@viruett.com>
2023-07-11 12:27:55 -0700

    updates header image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Welcome from '@/components/welcome';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-12 bg-light-green text-white">
+    <main className="flex min-h-screen flex-col items-center justify-between bg-light-green text-white">
       <Header/>
       <Welcome/>
       <Socials/>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,15 @@
 "use client"
+import { useState } from 'react';
 import Image from 'next/image';
 import logo from '../images/logo.png';
 import Button from './button';
 import Drawer from './drawer';
-import { useState } from 'react';
 
 export default function Header() {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <>
+        <div className="h-screen w-screen bg-[url('/summer.png')] bg-cover bg-bottom">
             <div id='header' className='container mx-auto max-w-5xl px-4 flex flex-row items-center justify-between'>
                 <div className='sm:block hidden'><Button title='shop' color='blue' url='https://opensea.io/collection/froggy-friends-2'/></div>
                 <div className='sm:block'><Image src={logo} alt='Froggy Friends' height={120}/></div>
@@ -17,10 +17,10 @@ export default function Header() {
             </div>
             <Drawer isOpen={isOpen} setIsOpen={setIsOpen}>
                 <div className='flex flex-col justify-between gap-4'>
-                    <p className='sm:text-5xl text-3xl'>Home</p>
-                    <p className='sm:text-5xl text-3xl text-light-gray'>Froggy Bridge</p>
-                    <p className='sm:text-5xl text-3xl'>Team</p>
-                    <p className='sm:block hidden sm:text-5xl text-3xl'>Shop</p>
+                    <a className='sm:text-5xl text-3xl' href='./'>Home</a>
+                    <a className='sm:text-5xl text-3xl text-light-gray' href=''>Froggy Bridge</a>
+                    <a className='sm:text-5xl text-3xl'>Team</a>
+                    <a className='sm:block hidden sm:text-5xl text-3xl' href='https://opensea.io/collection/froggy-friends-2' target='_blank'>Shop</a>
                     <div className='sm:hidden'>
                         <button className='bg-light-green hover:bg-green transition ease-in-out delay-150 hover:translate-y-2 hover:scale-110 rounded uppercase w-full h-full p-2'>
                             <a className='text-4xl' href='https://opensea.io/collection/froggy-friends-2' target='_blank'>Buy on Opensea</a>
@@ -28,6 +28,6 @@ export default function Header() {
                     </div>
                 </div>
             </Drawer>
-        </>
+        </div>
     )
 }
