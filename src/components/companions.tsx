de24b23c95bab144770524972918deaf713df880
Alfonso <alfonso@viruett.com>
2023-07-12 02:37:36 -0700

    updates companions

--- src/components/companions.tsx
+++ src/components/companions.tsx
@@ -6,11 +6,13 @@ export default function Companions() {
         <div className='bg-light-yellow w-full'>
             <div className='container mx-auto max-w-xl flex flex-col gap-10 items-center p-10 text-black'>
                 <p className='text-5xl'>Froggy Companions</p>
-                <p className='font-vista-sans'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <p className='font-vista-sans text-md sm:text-2xl uppercase'>
+                    Froggy Friends pioneered the first omnimetadata that allows frogs to pair with a companion that provides
+                    a staking boost.
+                </p>
+                <p className='font-vista-sans text-md sm:text-2xl uppercase'>
+                    Our genesis friend collection features five adorable companions to pair with your frog:
+                    Rabbit, Red Panda, Bear, Cat, Unicorn and Golden Tiger.
                 </p>
             </div>
             <div className='flex'>
--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -5,7 +5,7 @@ export default function Welcome() {
     return (
         <div className='w-full p-8 xl:p-24'>
             <div className='container mx-auto max-w-6xl flex flex-col xl:flex-row gap-10 items-center sm:p-10 text-black'>
-                <div className='flex flex-col gap-8'>
+                <div className='flex flex-col gap-10'>
                     <p className='text-5xl'>The Pond</p>
                     <p className='font-vista-sans text-md sm:text-2xl uppercase'>
                         The pond is home to the Froggy Friends brand and is a community of passionate individuals who believe in the mission of
