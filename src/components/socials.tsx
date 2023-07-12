a6a515088c08ae4ab3eb85aa9f08fae70171b0c9
Alfonso <alfonso@viruett.com>
2023-07-12 03:44:34 -0700

    adds instagram and tiktok

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -1,14 +1,17 @@
+import Image from "next/image";
+import instagram from '../images/instagram.png';
+import tiktok from '../images/tiktok.png';
+
 export default function Socials() {
     return (
-        <div className='bg-light-blue w-full p-8'>
-            <div className='container mx-auto max-w-xl flex flex-col gap-10 items-center p-10'>
-                <p className='text-5xl'>Froggy Friends on Socials</p>
-                <p className='font-vista-sans'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
+        <div className='bg-light-blue w-full'>
+            <div className='container mx-auto flex flex-col sm:flex-row gap-10 items-center p-10'>
+                <a href='https://www.instagram.com/froggyfriendsglobal/' target='_blank'>
+                    <Image src={instagram} alt='Instagram'/>
+                </a>
+                <a href='https://www.tiktok.com/@froggyfriendsglobal' target='_blank'>
+                    <Image src={tiktok} alt='Tiktok'/>
+                </a>
             </div>
         </div>
     )
