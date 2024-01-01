0c1210c36e70e9facdd34d170182c1fd5b16c1d9
0xFonzy <fonzy@fonzy.io>
2024-01-01 10:09:01 -0800

    updates discord link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,13 +8,13 @@ export default function Footer() {
             <div className='container mx-auto flex flex-col sm:flex-row justify-between items-center gap-10 p-10'>
                 <p className='text-2xl sm:text-3xl'>&copy; Froggy Friends 2023</p>
                 <div>
-                <Button title='Buy on Opensea' url='https://opensea.io/collection/froggyfriendsnft'/>
+                    <Button title='Buy on Opensea' url='https://opensea.io/collection/froggyfriendsnft' />
                 </div>
                 <div id='links' className='flex flex-row gap-4'>
-                    <Button title="twitter" icon={faTwitter} url="https://twitter.com/FroggyFriendNFT"/>
-                    <Button title="discord" icon={faDiscord} url="https://discord.gg/froggyfriends"/>
-                    <Button title="instagram" icon={faInstagram} url="https://www.instagram.com/froggyfriendsglobal"/>
-                    <Button title="tiktok" icon={faTiktok} url="https://www.tiktok.com/@froggyfriendsglobal"/>
+                    <Button title="twitter" icon={faTwitter} url="https://twitter.com/FroggyFriendNFT" />
+                    <Button title="discord" icon={faDiscord} url="https://discord.gg/BfPr9njdut" />
+                    <Button title="instagram" icon={faInstagram} url="https://www.instagram.com/froggyfriendsglobal" />
+                    <Button title="tiktok" icon={faTiktok} url="https://www.tiktok.com/@froggyfriendsglobal" />
                 </div>
             </div>
         </div>
--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -9,14 +9,14 @@ export default function Socials() {
             <div className='container mx-auto flex flex-col gap-10 items-center p-10'>
                 <p className='text-black text-5xl'>Froggy Socials</p>
                 <div className='mx-auto grid grid-rows-1 sm:grid-cols-2 md:grid-cols-3'>
-                    <a href='https://discord.gg/froggyfriends' target='_blank'>
-                        <Image src={discord} alt='Discord'/>
+                    <a href='https://discord.gg/BfPr9njdut' target='_blank'>
+                        <Image src={discord} alt='Discord' />
                     </a>
                     <a href='https://www.instagram.com/froggyfriendsglobal' target='_blank'>
-                        <Image src={instagram} alt='Instagram'/>
+                        <Image src={instagram} alt='Instagram' />
                     </a>
                     <a href='https://www.tiktok.com/@froggyfriendsglobal' target='_blank'>
-                        <Image src={tiktok} alt='Tiktok'/>
+                        <Image src={tiktok} alt='Tiktok' />
                     </a>
                 </div>
             </div>
