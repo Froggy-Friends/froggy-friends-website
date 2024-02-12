ce1ff126c5e7d024c2df7a8da9236169e76c38b4
Fonzy <fonzy@fonzy.io>
2024-02-12 14:46:31 -0800

    Update socials.tsx

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -9,9 +9,6 @@ export default function Socials() {
             <div className='container mx-auto flex flex-col gap-10 items-center p-10'>
                 <p className='text-black text-5xl'>Froggy Socials</p>
                 <div className='mx-auto grid grid-rows-1 sm:grid-cols-2 md:grid-cols-3'>
-                    <a href='https://discord.gg/BfPr9njdut' target='_blank'>
-                        <Image src={discord} alt='Discord' />
-                    </a>
                     <a href='https://www.instagram.com/froggyfriendsglobal' target='_blank'>
                         <Image src={instagram} alt='Instagram' />
                     </a>
