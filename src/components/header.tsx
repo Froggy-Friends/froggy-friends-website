3c8adb986f61d7527895d8ffa18e21197cc0885d
alfonsoaranzazu <alfonso@viruett.com>
2024-02-28 17:35:15 -0800

    adds magic eden shop link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
     return (
         <div className="flex flex-col gap-10 pt-2 h-screen w-screen bg-[url('/summer-mobile.png')] sm:bg-[url('/summer.png')] bg-cover bg-top">
             <div id='header' className='container mx-auto max-w-5xl px-4 flex flex-row items-center justify-between'>
-                <div className='sm:block hidden'><Button title='shop' url='https://opensea.io/collection/froggyfriendsnft' /></div>
+                <div className='sm:block hidden'><Button title='shop' url='https://magiceden.io/collections/ethereum/0x7ad05c1b87e93be306a9eadf80ea60d7648f1b6f' /></div>
                 <div className='sm:block'><Image className='rounded-full md:w-32 w-24 p-4' src={froggy} alt='Froggy Friends' height={120} /></div>
                 <div className='sm:block'><Button title='menu' onClick={() => setIsOpen(true)} /></div>
             </div>
@@ -26,7 +26,7 @@ export default function Header() {
             <Drawer isOpen={isOpen} setIsOpen={setIsOpen}>
                 <div className='flex flex-col justify-between gap-2'>
                     <a className='sm:text-4xl text-3xl hover:text-blue' href='./'>Home</a>
-                    <a className='sm:text-4xl text-3xl hover:text-blue' href='https://opensea.io/collection/froggyfriendsnft' target='_blank'>Shop</a>
+                    <a className='sm:text-4xl text-3xl hover:text-blue' href='https://magiceden.io/collections/ethereum/0x7ad05c1b87e93be306a9eadf80ea60d7648f1b6f' target='_blank'>Shop</a>
                     <a className='sm:text-4xl text-3xl text-light-gray cursor-not-allowed'>App</a>
                     <a className='sm:text-4xl text-3xl text-light-gray cursor-not-allowed'>IP Policy</a>
                     <a className='sm:text-4xl text-3xl text-light-gray cursor-not-allowed'>Privacy Policy</a>
