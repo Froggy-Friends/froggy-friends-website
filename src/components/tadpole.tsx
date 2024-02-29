9ccc9c5d1239bd2537cfa466de84b0dfb6b4dcbb
alfonsoaranzazu <alfonso@viruett.com>
2024-02-28 17:56:13 -0800

    updates tadpole import

--- src/components/tadpole.tsx
+++ src/components/tadpole.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import TadpolePfp from "../images/Tadpoles.png";
+import TadpolePfp from "../images/tadpole.png";
 
 export default function Tadpole() {
     return (
