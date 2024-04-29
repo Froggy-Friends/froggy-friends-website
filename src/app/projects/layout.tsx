08167becd86ac05df816e23e397dd3cae4d47788
0xFonzy <fonzy@fonzy.io>
2024-04-28 21:09:23 -0700

    adds react query

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -2,6 +2,7 @@ import '../globals.css'
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import type { Metadata } from 'next'
+import Provider from './provider';
 config.autoAddCss = false;
 
 export const metadata: Metadata = {
@@ -16,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className='font-hwt-artz'>{children}</body>
+      <body className='font-hwt-artz'>
+        <Provider>
+          {children}
+        </Provider>
+      </body>
     </html>
   )
 }
