6e631010b8c4e88d896c119dd2727dd5731a8d59
0xFonzy <fonzy@fonzy.io>
2024-04-28 21:09:40 -0700

    adds button type prop

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,22 +1,24 @@
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export interface ButtonProps {
-    title: string;
-    icon?: IconDefinition;
-    url?: string;
-    onClick?: any;
+type ButtonType = 'submit' | 'reset' | 'button' | undefined;
+type ButtonProps = {
+  title: string;
+  type?: ButtonType;
+  icon?: IconDefinition;
+  url?: string;
+  disabled?: boolean;
+  onClick?: any;
 }
 
 export default function Button(props: ButtonProps) {
-    const { title, url, icon, onClick } = props;
+  const { title, type, url, icon, disabled, onClick } = props;
 
-
-    return (
-        <button className='text-white bg-blue hover:bg-dark-blue transition ease-in-out delay-150 hover:translate-y-2 hover:scale-110 rounded uppercase w-full h-full p-4' onClick={onClick}>
-            <a href={url} target="_blank" className={`${icon ? '' : 'text-2xl sm:text-4xl'} tracking-widest`}>
-                {icon ? <FontAwesomeIcon icon={icon}/> : title}
-            </a>
-        </button>
-    )
+  return (
+    <button type={type} className='text-white enabled:bg-blue enabled:hover:bg-dark-blue disabled:bg-light-gray enabled:transition enabled:ease-in-out enabled:delay-150 enabled:hover:translate-y-2 enabled:hover:scale-110 disabled:cursor-not-allowed rounded uppercase w-full h-full p-4' disabled={disabled} onClick={onClick}>
+      <a href={url} target="_blank" className={`${icon ? '' : 'text-2xl sm:text-4xl'} tracking-widest`}>
+        {icon ? <FontAwesomeIcon icon={icon} /> : title}
+      </a>
+    </button>
+  )
 }
--- src/components/input.tsx
+++
@@ -1,8 +0,0 @@
-
-export function TextInput() {
-  return (
-    <input className='font-vista-sans w-full' type="text" placeholder="Enter text...">
-
-    </input>
-  )
-}
