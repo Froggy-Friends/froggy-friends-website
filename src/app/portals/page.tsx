c896224766bbbab1ae5bfa0a35dc3826003495b7
Jordan Baron <jordannbaron@gmail.com>
2024-06-14 10:29:35 -0400

    scale images down

--- src/app/portals/page.tsx
+++ src/app/portals/page.tsx
@@ -1,12 +1,20 @@
+import Image from "next/image";
+
 export default function Portals() {
   return (
     <div className="w-full h-screen bg-[url('/portal-bg.png')] bg-cover bg-center overflow-hidden">
-      <img
+      <Image
+        width={446}
+        height={448}
         src="/3d-portal.png"
+        alt="3D Portal"
         className="animate-scaleFadeIn absolute top-96 left-32"
       />
-      <img
+      <Image
+        width={491}
+        height={678}
         src="/pixel-portal.png"
+        alt="Pixel portal"
         className="animate-scaleFadeIn absolute right-0 top-36"
       />
     </div>
